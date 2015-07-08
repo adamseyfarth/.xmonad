62da9d6399e93f234fbc6e6828b7a78822b4a493
Adam Seyfarth <adam@seyfarth.name>
2015-07-07 20:51:47 -0500

    Finish an initial working config

--- xmonad.hs
+++ xmonad.hs
@@ -7,8 +7,16 @@ import System.IO
 
 main :: IO ()
 main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar /home/aseyfarth/.xmobarrc"
   xmonad $ defaultConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts  $  layoutHook defaultConfig
     , modMask = mod4Mask
-    }
+    , terminal = "urxvt"
+    , logHook = dynamicLogWithPP xmobarPP
+                { ppOutput = hPutStrLn xmproc
+                , ppTitle = xmobarColor "green" "" . shorten 50
+                }
+    } `additionalKeys`
+    [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+    ]
