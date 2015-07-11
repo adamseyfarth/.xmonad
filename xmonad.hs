0625ea5d2bee0680240a017b709700ff1eeb8185
Adam Seyfarth <adam@seyfarth.name>
2015-07-11 15:56:41 -0500

    Customize layouts
    - Put layouts into separate myLayouts symbol
    - Remove mirrored Tall
    - Add key (mod-b) to toggle covering of top bar

--- xmonad.hs
+++ xmonad.hs
@@ -5,12 +5,24 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+myLayouts = tiled ||| Full
+  where
+    tiled = Tall nmaster delta ratio
+    -- The default number of windows in the master pane
+    nmaster = 1
+
+    -- Default portion of screen occupied by master pane
+    ratio = 7/12
+
+    -- Portion of screen to increment by when resizing panes
+    delta = 1/60
+
 main :: IO ()
 main = do
   xmproc <- spawnPipe "/usr/bin/xmobar /home/aseyfarth/.xmobarrc"
   xmonad $ defaultConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    , layoutHook = avoidStruts myLayouts
     , modMask = mod4Mask
     , terminal = "urxvt"
     , logHook = dynamicLogWithPP xmobarPP
@@ -19,4 +31,6 @@ main = do
                 }
     } `additionalKeys`
     [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+    -- Allow/disallow covering of xmobar+trayer
+    , ((mod4Mask, xK_b), sendMessage ToggleStruts)
     ]
